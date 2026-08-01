655bb658bbf038bf2feb184357b26502a3eb2988
Alfredcollie <78186270+Alfredcollie@users.noreply.github.com>
2026-08-01 14:11:26 -0500

    Update api_movil.py

--- api_movil.py
+++ api_movil.py
@@ -66,7 +66,7 @@ async def subir_ticket_grifo(
             """
             
             respuesta = cliente_ia.models.generate_content(
-                model='gemini-2.0-flash', 
+                model='gemini-1.5-flash-8b', 
                 contents=[parte_imagen, prompt]
             )
             
